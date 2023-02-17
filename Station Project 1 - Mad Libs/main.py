6408dbd0ccfd8e6c0417c5ea52e608abfc941bf1
Err711 <ebayramyan@gmail.com>
2023-02-17 22:47:00 +0400

    first commit

---
+++ Station Project 1 - Mad Libs/main.py
@@ -0,0 +1,34 @@
+import random
+
+number_1 = ''
+number_2 = ''
+measure_of_time = ''
+mode_of_transportation = ''
+adjective_1 = ''
+adjective_2 = ''
+adjective_3 = ''
+noun_1 = ''
+noun_2 = ''
+noun_3 = ''
+noun_4 = ''
+color = ''
+part_of_the_body_1 = ''
+part_of_the_body_2 = ''
+verb = ''
+color = ''
+silly_word = ''
+
+TEMPLATES = {
+    1: f'It was about {number_1} {measure_of_time} ago when I arrived at the hospital in a {mode_of_transportation}.'
+       f'The hospital is a/an {adjective_1} place, there are a lot of {adjective_2} {noun_1} here. '
+       f'There are nurses here who have {color} {part_of_the_body_1}. '
+       f'If someone wants to come into my room I told them that they have to {verb} first. '
+       f'I’ve decorated my room with {number_2} {noun_2}.'
+       f'Today I talked to a doctor and they were wearing a {noun_3} on their {part_of_the_body_2}. '
+       f'I heard that all doctors {verb} {noun_4} every day for breakfast. '
+       f'The most {adjective_3} thing about being in the hospital is the {silly_word} {noun_1}!',
+    2: f'',
+    3: f''
+}
+
+
