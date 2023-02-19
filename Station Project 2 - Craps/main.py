13eb39f070c82219d93750758f87e2e7483c093f
Err711 <ebayramyan@gmail.com>
2023-02-19 19:09:15 +0400

    maybe the finel changes

--- Station Project 2 - Craps/main.py
+++ Station Project 2 - Craps/main.py
@@ -11,11 +11,19 @@ class Game:
         self.craps = [2, 3, 12]
         self.goal_numbers = [4, 5, 6, 8, 9, 10]
 
-    def game_process(self):
+    def start_game(self):
+        start = input('If you ready, type "start". ')
+        while start == 'Start' or start == 'start':
+            return f'{self.dice_1} + {self.dice_2} = {self.dice_sum}'
+        else:
+            raise ValueError("Type 'go' for start game. ")
 
+    def game_process(self):
         if self.dice_sum in self.user_numbers:
+            time.sleep(0.01)
             return "You Won!"
         elif self.dice_sum in self.craps:
+            time.sleep(0.01)
             return "Craps! \nCasino Wins!"
         else:  # elif self.dice_sum in self.goal_numbers:
             print(f"Now your goal number is {self.dice_sum}")
@@ -27,21 +35,15 @@ class Game:
                 time.sleep(1)
                 print(f'{self.dice_1} + {self.dice_2} = {goal_num_sum}')
                 if goal_num_sum == self.dice_sum:
+                    time.sleep(0.01)
                     return 'You won!'
                 elif goal_num_sum == 7:
+                    time.sleep(0.01)
                     return "Craps! \nCasino Wins!"
-
-    def start_game(self):
-        start = 'go'  # input('If you ready, type "go". ')
-        while start == 'go' or start == 'Go':
-            return f'{self.dice_1} + {self.dice_2} = {self.dice_sum}'
-        else:
-            raise ValueError("Type 'go' for start game. ")
 
 
 user_dice_1 = random.randint(1, 6)
 user_dice_2 = random.randint(1, 6)
-# user_input = input('If you ready, type "go". ')
 a = Game(user_dice_1, user_dice_2)
 print(a.start_game())
 print(a.game_process())
